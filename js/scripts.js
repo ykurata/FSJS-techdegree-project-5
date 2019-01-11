a05759c7ff64a7ccd3f09f280e8734ef9bd13e41
ykuruta <yasukokurata@gmail.com>
2019-01-11 13:28:54 -0500

    Show random employee photos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-$document.ready(function() {
-  
+$(document).ready(function() {
+  var userAPI = 'https://randomuser.me/api/?results=12';
+  $gallery = $('#gallery');
+
+  function displayPhotos(data) {
+
+    $.each(data.results, function(index, user) {
+      var cardHTML = $('<div class="card"></div>');
+
+      // Create and append employee's photo
+      var cardImageContainer = $('<div class="card-img-container"></div>');
+      var image = $('<img class="card-img" src="' + user.picture.large + '" alt="profile picture">');
+      cardImageContainer.append(image);
+      cardHTML.append(cardImageContainer);
+
+      // Create and append employee's info
+      var cardInfoContainer = $('<div class="card-info-container"></div>');
+      var name = $('<h3 id="name" class="card-name cap"></h3>').html(user.name.first);
+      var email = $('<p class="card-text"></p>').html(user.email);
+      var city = $('<p class="card-text cap"></p>').html(user.location.city);
+      cardInfoContainer.append(name);
+      cardInfoContainer.append(email);
+      cardInfoContainer.append(city);
+      cardHTML.append(cardInfoContainer);
+
+      // Append cardHTML to gallery div
+      $gallery.append(cardHTML);
+    }); // end each
+  }
+
+  $.getJSON(userAPI, displayPhotos);
 }); // end ready
